af6f53c540b92abd21248a87652ec5853ca8ee4f
Luyanda Mabozo <Luyandamabozo@gmail.com>
2020-08-21 15:15:30 +0200

    Removed Initial Route

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "We thank you for visiting our Sneakers API, WELCOME!!!" })
-);
-
 // Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
